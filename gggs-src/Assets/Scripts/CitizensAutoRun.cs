f38030012aa400720130c2115e036d18c42943a9
Nathan Wentworth <nwentworth95@gmail.com>
2017-03-03 13:51:22 -0500

    added base for humans walking around

---
+++ gggs-src/Assets/Scripts/CitizensAutoRun.cs
@@ -0,0 +1,43 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CitizensAutoRun : MonoBehaviour {
+
+  [SerializeField]
+  private float speedUpDistance;
+  [SerializeField]
+  private float defaultSpeed;
+  [SerializeField]
+  private float maxSpeedMultiplier;
+  [SerializeField]
+  private float centerOfMassYOffset;
+
+  private Rigidbody rb;
+
+  public float speed { get; set; }
+
+  private Transform kaiju;
+
+  private void Start() {
+
+    rb = GetComponent<Rigidbody>();
+
+    speed = defaultSpeed;
+    kaiju = GameObject.FindWithTag("Player").GetComponent<Transform>();
+    
+  }
+  
+  private void Update() {
+
+    float distance = Vector3.Distance(transform.position, kaiju.position);
+
+    if (distance < speedUpDistance) {
+      speed = (((1 - (distance / speedUpDistance)) * maxSpeedMultiplier) + defaultSpeed);
+    } else {
+      speed = defaultSpeed;
+    }
+		transform.Translate(Vector3.forward * Time.deltaTime * speed);
+	}
+
+}
