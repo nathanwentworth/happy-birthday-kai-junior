edb40487afffc75b7738e92abdfe229ebcfffb7a
Nathan Wentworth <nwentworth95@gmail.com>
2017-03-03 15:41:53 -0500

    edited bean men

--- gggs-src/Assets/Scripts/CitizensAutoRun.cs
+++ gggs-src/Assets/Scripts/CitizensAutoRun.cs
@@ -13,7 +13,7 @@ public class CitizensAutoRun : MonoBehaviour {
   [SerializeField]
   private float centerOfMassYOffset;
 
-  private Rigidbody rb;
+  private Rigidbody[] rb;
 
   public float speed { get; set; }
 
@@ -21,23 +21,45 @@ public class CitizensAutoRun : MonoBehaviour {
 
   private void Start() {
 
-    rb = GetComponent<Rigidbody>();
+    rb = GetComponentsInChildren<Rigidbody>();
 
     speed = defaultSpeed;
     kaiju = GameObject.FindWithTag("Player").GetComponent<Transform>();
     
   }
   
-  private void Update() {
+  private void FixedUpdate() {
 
-    float distance = Vector3.Distance(transform.position, kaiju.position);
+    if (Physics.Raycast(transform.position, -transform.up, 2f)) {
 
-    if (distance < speedUpDistance) {
-      speed = (((1 - (distance / speedUpDistance)) * maxSpeedMultiplier) + defaultSpeed);
-    } else {
-      speed = defaultSpeed;
+      float distance = Vector3.Distance(transform.position, kaiju.position);
+
+      if (distance < speedUpDistance) {
+        speed = (((1 - (distance / speedUpDistance)) * maxSpeedMultiplier) + defaultSpeed);
+      } else {
+        speed = defaultSpeed;
+      }
+
+      // get rotation of boy
+      // convert to vector 3
+      // set y rotation to 0
+      // 
+
+      Vector3 rotation = new Vector3(transform.rotation.x, 0, transform.rotation.z);
+
+      Quaternion q = Quaternion.Euler(rotation);
+
+      Vector3 forward = Vector3.zero;
+
+      forward = q * Vector3.forward;
+
+      Debug.Log(forward);
+
+  		transform.Translate(forward * Time.deltaTime * speed);
+      //rb[1].MovePosition(transform.position * (Time.deltaTime * speed));
+
     }
-		transform.Translate(Vector3.forward * Time.deltaTime * speed);
+
 	}
 
 }
