acdaf5626f5d1f05e226e15f91b4f9093ad63282
Nathan Wentworth <nwentworth95@gmail.com>
2017-03-08 17:30:43 -0500

    added game icon, edited prefab values

--- gggs-src/Assets/Scripts/RigidbodySleepCheck.cs
+++ gggs-src/Assets/Scripts/RigidbodySleepCheck.cs
@@ -50,7 +50,7 @@ public class RigidbodySleepCheck : MonoBehaviour {
             hudManager.HighScoreChange();
           }
 
-          StartCoroutine(CheckMoveState());
+          // StartCoroutine(CheckMoveState());
 
         }
       }
