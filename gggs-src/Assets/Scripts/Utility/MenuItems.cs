a0ebbf3138ae7f49937c9b2ac2971447f50cfb8b
Nathan Wentworth <nwentworth95@gmail.com>
2017-03-28 13:28:27 -0400

    added object behaviors script, consolodating all other object scripts into one

--- gggs-src/Assets/Scripts/Utility/MenuItems.cs
+++ gggs-src/Assets/Scripts/Utility/MenuItems.cs
@@ -25,6 +25,9 @@ public class MenuItems : MonoBehaviour {
       if (go.GetComponent<ObjectDataContainer>() == null) {
         go.AddComponent<ObjectDataContainer>();
       }
+      if (go.GetComponent<ObjectBehaviors>() == null) {
+        go.AddComponent<ObjectBehaviors>();
+      }
 
     }
   }
