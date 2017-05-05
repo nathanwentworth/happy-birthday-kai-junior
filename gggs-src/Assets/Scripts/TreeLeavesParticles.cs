9546e078e0c853fe06c474840d2ae04c644ab904
Sabrina <sabrinavelez777@gmail.com>
2017-05-05 16:33:17 -0400

    Added particle tree script

--- gggs-src/Assets/Scripts/TreeLeavesParticles.cs
+++ gggs-src/Assets/Scripts/TreeLeavesParticles.cs
@@ -2,15 +2,38 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class TreeLeavesParticle : MonoBehaviour {
+public class TreeLeavesParticles : MonoBehaviour {
 
-	// Use this for initialization
-	void Start () {
-		
+	private List<GameObject> treeParticles = new List<GameObject>();
+
+	[SerializeField]
+	private int particlesToSpawn = 10;
+	[SerializeField]
+	private GameObject particlePrefab;
+
+
+	private void Start () {
+		if (particlePrefab == null) {
+			Debug.LogWarning ("You need to attach a prefab to the TreeLeavesParticles script!");
+			return;
+		}
+
+		for (int i = 0; i < particlesToSpawn; i++) {
+			GameObject obj = (GameObject)Instantiate (particlePrefab);
+			obj.SetActive (false);
+			treeParticles.Add (obj);
+		}
 	}
 	
-	// Update is called once per frame
-	void Update () {
-		
+
+	private void OnCollisionEnter(Collision other) {
+		if (particlePrefab == null) { return; }
+		foreach (GameObject obj in treeParticles) {
+			if (!obj.activeInHierarchy) {
+				obj.transform.position = other.contacts[0].point;
+				obj.SetActive (true);
+				obj.GetComponent<ParticleSystem>().Play();
+			}
+		}
 	}
 }
