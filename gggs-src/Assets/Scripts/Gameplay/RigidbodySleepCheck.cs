299f1b668380380bdd3af66b91fd2751ade12f0e
nathan wentworth <nwentworth95@gmail.com>
2017-05-09 17:59:09 -0400

    cleaned up rigidbody sleep check

--- gggs-src/Assets/Scripts/Gameplay/RigidbodySleepCheck.cs
+++ gggs-src/Assets/Scripts/Gameplay/RigidbodySleepCheck.cs
@@ -15,9 +15,7 @@ public class RigidbodySleepCheck : MonoBehaviour {
   private readonly Color hitColor = new Color(0.8F, 0.8F, 0.8F, 1F);
 
   private void Start() {
-
     objName = gameObject.name;
-
 
     if (objName.LastIndexOf(" ") > 0) {
         objName = objName.Substring(0, objName.LastIndexOf(" "));
@@ -47,19 +45,6 @@ public class RigidbodySleepCheck : MonoBehaviour {
     scoreTextPopup = FindObjectOfType(typeof(ScoreTextPopup)) as ScoreTextPopup;
 
     StartCoroutine(GetObjectProperties());
-
-
-    // MeshFilter objMesh = null;
-    // float volume = -1;
-    // if ((objMesh = GetComponent<MeshFilter>()) != null) {
-    //     volume = VolumeOfMesh(objMesh.mesh);
-    // }
-
-
-    // volume *= ((transform.localScale.x + transform.localScale.y + transform.localScale.z) / 3);
-
-    // Debug.Log(objName + " volume: " + volume);
-
   }
 
   private void FixedUpdate() {
@@ -110,29 +95,6 @@ public class RigidbodySleepCheck : MonoBehaviour {
         StartCoroutine(DestroyObject());
       }
     }
-  }
-
-  public float SignedVolumeOfTriangle(Vector3 p1, Vector3 p2, Vector3 p3) {
-    float v321 = p3.x * p2.y * p1.z;
-    float v231 = p2.x * p3.y * p1.z;
-    float v312 = p3.x * p1.y * p2.z;
-    float v132 = p1.x * p3.y * p2.z;
-    float v213 = p2.x * p1.y * p3.z;
-    float v123 = p1.x * p2.y * p3.z;
-    return (1.0f / 6.0f) * (-v321 + v231 + v312 - v132 - v213 + v123);
-  }
-
-  public float VolumeOfMesh(Mesh mesh) {
-    float volume = 0;
-    Vector3[] vertices = mesh.vertices;
-    int[] triangles = mesh.triangles;
-    for (int i = 0; i < mesh.triangles.Length; i += 3) {
-      Vector3 p1 = vertices[triangles[i + 0]];
-      Vector3 p2 = vertices[triangles[i + 1]];
-      Vector3 p3 = vertices[triangles[i + 2]];
-      volume += SignedVolumeOfTriangle(p1, p2, p3);
-    }
-    return Mathf.Abs(volume);
   }
 
   private IEnumerator DestroyObject() {
