c47a8a7ad4e6e04b41e4cc6a129a82c91c3d0ad5
nathan wentworth <nwentworth95@gmail.com>
2017-05-10 16:46:30 -0400

    added building collision sound

---
+++ gggs-src/Assets/Scripts/BuildingSoundTrigger.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BuildingSoundTrigger : MonoBehaviour {
+
+  private AudioSource audio;
+  [SerializeField]
+  private AudioClip[] clips;
+
+	private void Start () {
+    audio = transform.root.GetComponent<AudioSource>();
+	}
+
+  private void OnCollisionEnter(Collision other) {
+    if (audio != null && other.gameObject.tag == "Player") {
+      StartCoroutine(PlayAudio());
+    }
+  }
+
+  private IEnumerator PlayAudio() {
+    if (clips.Length > 0) {
+      audio.clip = clips[Random.Range(0, clips.Length)];
+    }
+
+    audio.Play();
+    Debug.Log("Audio clip " + audio.clip.name + " played");
+    yield return null;
+  }
+
+}
